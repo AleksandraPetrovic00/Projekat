cd59189202c7a07a4818168f2b9b24118dc2bd95
AleksandraPetrovic00 <jovinagimnazija>
2021-06-26 16:13:13 +0200

    directory change

---
+++ src/main/resources/static/js/zahteviZaRegistraciju.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8090/api/korisnici/zahtevi",
+        dataType: "json",
+        success: function(response) {
+            console.log("USPESNO", response);
+            console.log(response);
+
+            for(let korisnik of response){
+                let row = "<tr>";
+                row+= "<td>" + korisnik.id + "</td>";
+                row+= "<td>" + korisnik.korisnickoIme + "</td>";
+                row+= "<td>" + korisnik.lozinka + "</td>";
+                row+= "<td>" + korisnik.ime + "</td>";
+                row+= "<td>" + korisnik.prezime + "</td>";
+                row+= "<td>" + korisnik.kontaktTelefon + "</td>";
+                row+= "<td>" + korisnik.emailAdresa + "</td>";
+                row+= "<td>" + korisnik.datumRodjenja + "</td>";
+                row+="<td><input type='checkbox' data='"+korisnik.id+"'/></td>"
+                row+="</tr>";
+
+                $('.content-table').append(row);
+            }
+        },
+        error: function(response) {
+            console.log("ERROR: ", response);
+        }
+    });
+$("#potvrdi").on("click", function(event){
+    event.preventDefault();
+    let ids=[]
+    $("[type='checkbox']").each(function(elem){
+        console.log(this)
+        if($(this).is(":checked")){
+            ids.push($(this).attr("data"));
+            window.location.href="odobravanjeZahtevaTrenera.html"
+        }
+    })
+    $.ajax({
+        type:"PUT",
+        url:"http://localhost:8090/api/korisnici/accept",
+        sataType:"json",
+        contentType: "application/json",
+        data: JSON.stringify(ids)
+    });
+})
+
+});
