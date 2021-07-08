7b508a1ea7f21067ebecdcac28730a8b79fdb1db
AleksandraPetrovic00 <jovinagimnazija>
2021-07-08 20:21:10 +0200

    odradjeni ocenjeni termini commit

--- src/main/java/ac/rs/uns/ftn/fitnescentar/model/Ocena.java
+++ src/main/java/ac/rs/uns/ftn/fitnescentar/model/Ocena.java
@@ -11,7 +11,7 @@ public class Ocena implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private int ocena;
 
     @ManyToOne(fetch = FetchType.EAGER)
